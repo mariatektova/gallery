fe85f60ed6a2802887c3dd714cc3b37f34a05e4c
maria tektova <tkmarie@yandex.ru>
2022-06-21 15:00:01 +0300

    lightbox created

---
+++ lightbox.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+const lightboxBg = document.createElement("div");
+lightboxBg.id = "lightboxBg";
+document.body.appendChild(lightboxBg);
+
+const images = document.querySelectorAll('.zoom_img');
+images.forEach(image => {
+	image.addEventListener('click', e => {
+		lightboxBg.classList.add('active');
+		const lightboxImg = document.createElement('img');
+		lightboxImg.src = image.src;
+		lightboxImg.id = "lightboxImg";
+		while (lightboxBg.firstChild){
+			lightboxBg.removeChild(lightboxBg.firstChild)
+		}
+		lightboxBg.appendChild(lightboxImg);
+	})
+})
+
+lightboxBg.addEventListener('click', e =>{
+	lightboxBg.classList.remove('active');
+})
+})
